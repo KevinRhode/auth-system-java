390034630f6c6d414d407b0e6cc3def0fab68f83
Kevin Rhode <kevinrhode@hotmail.com>
2026-05-24 21:59:12 -0400

    updates

--- backend/src/main/java/com/authsystemjava/backend/controller/HealthController.java
+++ backend/src/main/java/com/authsystemjava/backend/controller/HealthController.java
@@ -1,12 +1,12 @@
 package com.authsystemjava.backend.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequiredArgsConstructor
 public class HealthController  {
 
     @Value("${cors.allowed-origins}")
