80be2cd2f7c77e416c7ca9ab3f8033294d51eeec
Kevin Rhode <kevinrhode@hotmail.com>
2026-05-24 22:31:11 -0400

    update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,13 +6,13 @@ export const routes: Routes = [
   {
     path: 'login',
     title: 'Auth System Java',
-    loadComponent: () => import('./auth/login/login.component.ts')
+    loadComponent: () => import('./auth/login/login.component')
       .then(m => m.LoginComponent)
   },
   {
     path: 'register',
     title: 'Auth System Java',
-    loadComponent: () => import('./auth/register/register.component.ts')
+    loadComponent: () => import('./auth/register/register.component')
       .then(m => m.RegisterComponent)
   },
   {
